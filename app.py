b7a0fa460a1797699597fa7d303a02ebfd2d9640
Prov2 <geraldzimmerman81@gmail.com>
2026-05-22 07:54:06 -0500

    Fix indentation for tab2 context manager

--- app.py
+++ app.py
@@ -25,5 +25,5 @@ if location and location.get("latitude"):
     # 3. UI Tabs
     tab1, tab2, tab3 = st.tabs(["Current", "Radar", "Settings"])
     
-          with tab2:
+     with tab2:
               
